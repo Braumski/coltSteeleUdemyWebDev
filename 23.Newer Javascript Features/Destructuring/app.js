fdbf5117cb53b7c7e9d1361b63e587c37a18a73e
Braumski <codyphillips@ymail.com>
2022-03-04 19:25:08 -0600

    blah

--- 23.Newer Javascript Features/Destructuring/app.js
+++ 23.Newer Javascript Features/Destructuring/app.js
@@ -50,5 +50,91 @@ const firstName = user.firstName;
 const lastName = user.lastName;
 const born = user.email;
 
-// But with the destructuring syntax, you can do it like this
+// But with the destructuring syntax, you can do it like this.
+
 const{email, firstName, lastName, city, bio}= user;
+
+// This example above will name your variables exactly the same as the objects property name.
+//  email = 'harvey@gmail.com'
+//  firstName = 'Harvey'
+// etc.
+
+// If you wanted your destructured variables to use a different name than the one used in the object,
+// you would have to use a sligthly different syntax including a colon.
+
+const { born: birthYear, died: deathYear} = user;
+// birthYear = '1930'
+// deathYear = '1978'
+
+// In this example, you add a colon to the properties that you want to be named differently than the
+// original property. In the example, I don't want to extract the 'born' property and keep the name
+// as 'born', I want the name to be different, so I used a colon and set it to birthYear. Now, I have
+// a variable called birthYear = '1930'
+
+// Same for the 'died' property, I changed its name to deathYear. So now I have a variable called deathYear = '1978'
+
+// Setting default placeholders in properties ///////////////////////////////////////////////////////////////////////////////////
+
+
+// Two objects representing users in your database
+
+const user = {
+    email: 'harvey@gmail.com',
+    password: 'asdwadjkna123',
+    firstName: 'Harvey',
+    lastName: 'Milk',
+    born: '1930',
+    died: '1978',
+    bio: 'Harvey Bernard Milk was an american politician',
+    city: 'San Fransisco',
+    state: 'California'
+}
+
+const user2 = {
+    email: 'zinatarenae890@gmail.com',
+    password: 'joshla56',
+    firstName: 'Renae',
+    lastName: 'Zinata',
+    born: '1930',
+    city: 'Tulsa',
+    state: 'Oklahoma'
+}
+
+// Notice how user2 does not have a "died" property, because they haven't died yet , they also
+// dont have a bio because they weren't widely recognized.
+
+// If you were to attempt to destructure "died" from user2, because you had no idea what the data looked like
+// and you didn't know they were still alive, you would get "undefined" in return.
+
+const {firstName, lastName, died} = user2;
+
+// firstName = "Renae"
+// lastName = "Zinata"
+// died = undefined
+
+// The variable "died" os made, but its value is undefined
+
+// To remedy this issue, you can set a default that would be used if there were no value there,
+// exactly like you would set a default in an argument.
+
+const {firstName, lastName, died = 'N/A'} = user2;
+
+// firstName = "Renae"
+// lastName = "Zinata"
+// died = 'N/A'
+
+// Note: the default value set with "=" will ONLY be used if there is no property, or value to the property that exists.
+
+
+// Destructuring params  //////////////////////////////////////////////////////////////////////////////
+
+const fullName = ({first, last}) => {
+    return  `${first} ${last}`
+}
+const runner = {
+    first: "Eliud",
+    last: "Kipchoge",
+    country: "Kenya"
+}
+
+fullName(runner); //"Eliud Kipchoge"
