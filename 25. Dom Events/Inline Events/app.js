0468d080f7ce1be3a7c6b2f43045313775a96c93
Braumski <codyphillips@ymail.com>
2022-03-12 15:24:15 -0600

    update

---
+++ 25. Dom Events/Inline Events/app.js
@@ -0,0 +1,21 @@
+const btn = document.querySelector('#v2');
+
+btn.onclick = function(){
+    console.log('You clicked me');
+    console.log('I hope it worked');
+}
+
+function scream(){
+    console.log('aaaaaa');
+    console.log('stop that');
+}
+
+// None of the functions above are being used, only defined.
+// Below, we are going to use them.
+btn.onmouseenter = scream;
+//on mouse enter just means when the mouse enters the button.
+
+
+document.querySelector('h1').onclick = function() {
+        alert('you clicked the h1');
+}
